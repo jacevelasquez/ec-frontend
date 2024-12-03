b85f52528f7e526820890f52e06512ec2b8c63cc
jacevelasquez <44248245+jacevelasquez@users.noreply.github.com>
2024-12-04 00:43:33 +0800

    added sprite route

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -4,4 +4,8 @@ export const handleMenuClick = (menuName, setActiveMenu) => {
 
 export function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+};
+
+export function formatNumber(number) {
+    return number.toString().padStart(3, "0");
+};
